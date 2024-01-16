f474d172fabfd8c694ded92b00c430102bcf15dc
Rasmus E <fatman00hot@hotmail.com>
2024-01-16 14:50:38 +0100

    changed login code and README file

--- dnac/Login.py
+++ dnac/Login.py
@@ -11,16 +11,18 @@ st.title("Login page")
 st.sidebar.success("Select a page above")
 
 if "dnac" not in st.session_state:
-
+    url = st.text_input("Server IP/name")
     username = st.text_input("Username")
     password = st.text_input("Password", type='password')
 
 
-if st.button("Login"):
+btn_login = st.button("Login")
+
+if btn_login:
     try:
         dnac = api.DNACenterAPI(username=username,
                             password=password,
-                            base_url="https://dnac.dccat.dk:443",
+                            base_url=f"https://{url}:443",
                             version='2.3.5.3',
                             verify=False)
         st.success("Token: "+dnac.access_token)
@@ -28,3 +30,6 @@ if st.button("Login"):
     except:
         st.error("Authentication Error")
         st.stop()
+
+if st.button("Logout"):
+    del st.session_state["dnac"]
---
+++ dnac/README.md
@@ -0,0 +1,17 @@
+# DNAC Tools
+
+### Versions
+Python: 3.9.2
+DNAC: 2.3.5.5-70026
+### Description
+This script will run a streamlit website where you can login to DNAC, and insert a list of IP ranges in a textbox.
+This list will be created as individual discovery jobs as DNAC only support ranges of max 4096 IPs in one job.
+
+* Start the webpage using streamlit
+* Access the page using a browser on port 8501
+* Login to the DNAC
+* Go to discovery and fill the text area
+### 
+```
+streamlit run Login.py
+```
